3041027eae9df16ae56aef7ece2af6638d8f33b0
Lara <volynkinala@gmail.com>
2022-12-14 13:19:41 +0400

    init: start online-store-team task

---
+++ src/Components/header/index.tsx
@@ -0,0 +1,16 @@
+// import { NavLink } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <div>
+      {/* <nav>
+        <NavLink to="/">
+          Main
+        </NavLink>
+        <NavLink to="/product">
+          Product
+        </NavLink>
+      </nav> */}
+    </div>
+  );
+}

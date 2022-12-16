6137ce536d25b43c727fbd357fe15d5f3d464b23
Lara <volynkinala@gmail.com>
2022-12-16 14:23:12 +0400

    feat: add header markup

--- src/Components/header/index.tsx
+++ src/Components/header/index.tsx
@@ -1,16 +1,20 @@
 // import { NavLink } from 'react-router-dom';
+import './index.scss';
+import { NavigateFunction, useNavigate } from 'react-router-dom';
 
 export default function Header() {
+  const navigate: NavigateFunction = useNavigate();
   return (
-    <div>
-      {/* <nav>
-        <NavLink to="/">
-          Main
-        </NavLink>
-        <NavLink to="/product">
-          Product
-        </NavLink>
-      </nav> */}
-    </div>
+    <header className="header">
+      <button type="button" onClick={() => navigate('/')} className="header__logo">Online Store</button>
+      <p className="header__cart-total">
+        Cart total:
+        {' '}
+        <span>$0</span>
+      </p>
+      <p className="header__cart">
+        0
+      </p>
+    </header>
   );
 }

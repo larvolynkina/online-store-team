d469906ab6273eeb0c38e4ea84407976d6cc12d8
Lara <volynkinala@gmail.com>
2022-12-19 20:33:37 +0400

    feat: add brand and category filter draft

--- src/Components/ProductFilterList/ProductFilterList.tsx
+++ src/Components/ProductFilterList/ProductFilterList.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
 import './ProductFilterList.scss';
+import ProductFilter from '../ProductFilter/ProductFilter';
+import useFetch from '../../Hooks/useFetch';
+import { IProduct } from '../../types';
 
 function ProductFilterList() {
+  const { products } = useFetch();
+  const categories: string[] = [...new Set(products.map((item: IProduct) => item.category))];
+  const brands: string[] = [...new Set(products.map((item: IProduct) => item.brand))];
+
   return (
-    <div className="filter-list">ProductFilterList</div>
+    <div className="filter-list">
+      <ProductFilter name="Category" list={categories} />
+      <ProductFilter name="Brand" list={brands} />
+    </div>
   );
 }
 
--- src/Pages/Main/index.tsx
+++ src/Pages/Main/index.tsx
@@ -1,22 +1,64 @@
+import React, { createContext } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import ProductsMain from '../../Components/ProductsMain/ProductsMain';
 import useFetch from '../../Hooks/useFetch';
+import ProductFilterList from '../../Components/ProductFilterList/ProductFilterList';
+import './index.scss';
 import { IProduct } from '../../types';
 
 type MainProps = {
   headerRender: () => void;
-}
+};
+
+// interface IFilterContextObject {
+//   filtered: IProduct[],
+// }
+
+// type TFilter = {
+//   [key: string]: string;
+// };
+
+export const FilterContext = createContext<IProduct[]>([]);
 
 function Main({ headerRender }: MainProps) {
+  const [searchParams] = useSearchParams();
   const { products } = useFetch();
-  const categories: string[] = [...new Set(products.map((item: IProduct) => item.category))];
-  const brands: string[] = [...new Set(products.map((item: IProduct) => item.brand))];
-  console.log(categories, brands);
+
+  function isFiltered(arr: IProduct[]) {
+    const categoryfiltered: IProduct[][] = [];
+    const brandFiltered: IProduct[][] = [];
+    if (searchParams.toString()) {
+      const categoryFilters = searchParams.getAll('category');
+      if (categoryFilters.length > 0) {
+        categoryFilters.forEach((filter: string) => {
+          categoryfiltered.push(arr.filter((product: IProduct) => product.category === filter));
+        });
+      } else {
+        categoryfiltered.push(arr);
+      }
+      const brandFilters = searchParams.getAll('brand');
+      if (brandFilters.length > 0) {
+        brandFilters.forEach((filter: string) => {
+          brandFiltered.push(
+            categoryfiltered.flat().filter((product: IProduct) => product.brand === filter),
+          );
+        });
+        return brandFiltered.flat();
+      }
+      return categoryfiltered.flat();
+    }
+    return arr;
+  }
+
+  const filtered = isFiltered(products);
 
   return (
-    <>
-      <h1>Main</h1>
-      <ProductsMain headerRender={headerRender} products={products} />
-    </>
+    <FilterContext.Provider value={filtered}>
+      <div className="container">
+        <ProductFilterList />
+        <ProductsMain headerRender={headerRender} products={filtered} />
+      </div>
+    </FilterContext.Provider>
   );
 }
 
